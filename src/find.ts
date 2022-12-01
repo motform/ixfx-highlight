3e2ee12d93316dc059c503dc078993769b8aa7df
motform <love.lagerkvist@protonmail.com>
2022-12-01 17:25:32 +0100

    Don't try to match empty identifier rages.

--- src/find.ts
+++ src/find.ts
@@ -1,8 +1,9 @@
 import * as vscode from "vscode";
 import { Manager } from "./shared-types";
 
-export const state = ["state", "useState", "updateState", "drawState"]; // TODO make this into a regex instaed
-export const settings = ["settings", "useSettings", "updateSettings"];
+export const state = new Set(["state", "useState", "updateState", "drawState"]); // TODO make this into a regex instaed
+export const settings = new Set(["settings", "useSettings", "updateSettings"]);
+
 
 const identifier = /[a-zA-Z]([a-zA-Z]|\d)*/g;
 const objectDestructuring = /\{.*\}/g;
@@ -30,18 +31,21 @@ export function variablesDestructuredFrom(manager: Manager, editor: vscode.TextE
  * Return a RegExp or-ing the strings in strs, appending legal next tokens (like `foo.` or `foo;`).
  * Example: ["foo", "bar"] => /foo|bar/g
  */
-function concatOrRegex(strs: Iterable<string>): RegExp {
-    const re = Array.from(strs).map(s => `${s}(\\W|\\n){1}`).join("|"); // NOTE: This does not match an identifier followed by nothing. This can happen if the identifier is the last token in the file, but that case is farily unlikely in real world usage.
-    return new RegExp(re, "g");
+function concatOrRegex(identifiers: Set<string>): RegExp {
+    if (identifiers.size === 0) return /(?:)/;
+
+    const regex = Array.from(identifiers).map(s => `${s}(\\W|\\n){1}`).join("|"); // NOTE: This does not match an identifier followed by nothing/EOF. This can happen if the identifier is the last token in the file, but that case is farily unlikely in real world usage.
+    return new RegExp(regex, "g");
 }
 
 /**
  * Return the ranges where identifiers occur in the current buffer.
  */
-export function rangesMatching(identifiers: Iterable<string>, editor: vscode.TextEditor): vscode.Range[] {
+export function rangesMatching(identifiers: Set<string>, editor: vscode.TextEditor): vscode.Range[] {
     const ranges: vscode.Range[] = [];
+    const regex = concatOrRegex(identifiers);
 
-    const matches = editor.document.getText().matchAll(concatOrRegex(identifiers));
+    const matches = editor.document.getText().matchAll(regex);
     for (const match of matches) {
         if (!match.index) continue;
         const start = editor.document.positionAt(match.index);
